d45c3833a7b51c2d36e24ae95bea407cc4bae266
Naski <jonas.marowski@smail.th-koeln.de>
2022-02-16 00:00:41 +0100

    renaming functions

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Event } from './interfaces/event.interface'
 
+let events: Events[];
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getEvents(): string {
+    return events;
   }
 }
