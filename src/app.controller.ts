56c9bd7cac6dc2198bbee97f4925b2bc15dda679
Naski <jonas.marowski@smail.th-koeln.de>
2022-02-16 01:04:07 +0100

    controller logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Event } from './interfaces/event.interface'
+import { EventDto } from './interfaces/event.dto'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/events')
   getEvents(): Event[] {
     return this.appService.getEvents();
+  }
+  @Post('/events')
+  addEvent(@Body() message: EventDto) {
+    if(message.eventDate < 1600000000 || message.eventCity.length <= 6) {
+      return null;
+    }
+    return this.appService.addEvent(message)
   }
 }
