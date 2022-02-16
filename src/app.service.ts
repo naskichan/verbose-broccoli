a83243a1a033f1d7c68cbdc0d7ba35a47ad569a7
Naski <jonas.marowski@smail.th-koeln.de>
2022-02-16 01:03:54 +0100

    add Event functionality

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Event } from './interfaces/event.interface'
+import { EventDto } from './interfaces/event.dto'
+import { db } from './data.mock'
 
-let events: Event[];
 @Injectable()
 export class AppService {
+  events = db;
   getEvents(): Event[] {
-    return events;
+    console.log(this.events)
+    return this.events;
+  }
+  addEvent(newEvent: EventDto): Event {
+    let obj: Event = {eventTitle: newEvent.eventTitle, eventCity: newEvent.eventCity, eventDate: newEvent.eventDate, tickets: []}
+    this.events.push(obj)
+    return obj;
   }
 }
