d2a0a4a02294b37878a4bb4e30def1c1a9218a22
Naski <jonas.marowski@smail.th-koeln.de>
2022-02-16 13:40:01 +0100

    basic layouting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,33 @@
 import styled from '@emotion/styled'
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
 import axios from 'axios'
+import Button from '@mui/material/Button';
 
 
 function App() {
   return (
+    <Container>
+      <Box sx={{ bgcolor: '#cfe8fc', height: '100vh'}}>
+        <Stack spacing={2}>
     <BodyWrapper>
       <p>Hello</p>
     </BodyWrapper>
+        </Stack>
+      </Box>
+    </Container>
   );
 }
 const BodyWrapper = styled.div`
-  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 8rem;
+`
+const EventsList = styled.div`
+  
 `
 
 export default App;
