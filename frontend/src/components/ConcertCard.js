7fe1febad20faec3a540014aa6fae8d372f6d1a1
Naski <jonas.marowski@smail.th-koeln.de>
2022-03-01 13:18:06 +0100

    alignment fixes

--- frontend/src/components/ConcertCard.js
+++ frontend/src/components/ConcertCard.js
@@ -7,12 +7,13 @@ import Button from '@mui/material/Button';
 
 function ConcertCard(props) {
     const date = new Date(props.date*1000)
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     return (
-        <Card sx={{ display: 'flex', justifyContent: "space-between", maxHeight: "8rem", margin: "3rem"}}>
+        <Card sx={{ display: 'flex', justifyContent: "space-between", margin: "1rem 0rem 1rem 0rem", maxHeight: "8rem", width: "100%"}}>
           <CardMedia
             component="img"
             image="https://source.unsplash.com/random?concert"
-            sx={{ maxWidth: "18rem"}}
+            sx={{ maxWidth: "18rem", maskImage: "linear-gradient(to right, rgba(0,0,0,1), rgba(0,0,0,0))"}}
             alt="image of a concert"
           />
           <CardContent sx={{ flexGrow: "1" }}>
@@ -20,13 +21,13 @@ function ConcertCard(props) {
               {props.title}
             </Typography>
             <Typography variant="body1" align="left">
-              {date.getDate()+"."+(date.getMonth()+1)+"."+date.getFullYear()}
+              {weekday[date.getDay()]+" the "+date.getDate()+"."+(date.getMonth()+1)+"."+date.getFullYear()}
             </Typography>
             <Typography variant="body1" align="left">
                 At: {props.city}
             </Typography>
           </CardContent>
-          <CardActions>
+          <CardActions sx={{ display: "flex", flexDirection: "column", justifyContent: "space-around"}}>
             <Button onClick={props.onClick} variant='contained' size="small">Book</Button>
             <Typography variant="body1" align="left">
               {props.ticketCount} {props.ticketCount == 1 ? ("ticket") : ("tickets")} already sold
