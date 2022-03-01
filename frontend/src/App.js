2d26ba9ca0449f5e74284753e6643d656d6fb19b
Naski <jonas.marowski@smail.th-koeln.de>
2022-03-01 13:18:18 +0100

    proper visual splitup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import styled from '@emotion/styled'
 import Container from '@mui/material/Container'
+import Divider from '@mui/material/Divider'
 import Box from '@mui/material/Box'
 import ConcertCard from './components/ConcertCard'
+import AddCard from './components/AddCard'
 
 
 function App() {
@@ -26,16 +28,19 @@ function App() {
       setShouldUpdate(current => !current)
     })
   }
+  function addEvent(values) {
+    return null
+  }
   return (
-    <Container>
-      <Box sx={{ padding: "4rem"}}>
+    <Container sx={{ marginTop: "8rem" }}>
+      <Content>
         {events.map((event, index) => (
           <ConcertCard key={index} title={event.eventTitle} date={event.eventDate} city={event.eventCity} ticketCount={event.tickets.length} onClick={() => book(index)} />
         ))}
-      </Box>
-      <Box sx={{ padding: "4rem"}}>
+      </Content>
+      <Content>
         <AddCard onClick={addEvent}/>
-      </Box>
+      </Content>
     </Container>
   );
 }
@@ -43,10 +48,17 @@ const BodyWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  margin: 8rem;
-`
-const EventsList = styled.div`
   
+`
+const Content = styled.div`
+  background-color: #424242;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 3rem 4rem 3rem 4rem;
+  border-radius: 8px;
+  margin: 2rem;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 `
 
 export default App;
