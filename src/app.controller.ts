d51582b1f6105a765f65d281a9ca3e12b421e1ab
Naski <jonas.marowski@smail.th-koeln.de>
2022-03-04 09:49:12 +0100

    improving request feedback and input validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,7 +14,10 @@ export class AppController {
   }
   @Post('/events')
   addEvent(@Body() message: EventDto) {
-    if(message.eventDate < Date.now()) {
+    if(message.eventCity == undefined || message.eventDate == undefined || message.eventTitle == undefined) {
+      throw new BadRequestException("Cannot sumbit undefined data. Props: {'eventTitle': 'example', 'eventDate': 0, 'eventCity': 'example'")
+    }
+    else if(message.eventDate < Date.now()) {
       throw new BadRequestException("Events cannot be in the past, make sure to use a timestamp in miliseconds")
     } else if(message.eventCity.length < 6 || message.eventTitle.length < 6) {
       throw new BadRequestException("Event names and Cities must have at least 6 characters")
